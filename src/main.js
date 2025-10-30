63269254971b9dc72b9ab8e907edfd968309cf49
jesperringhog <jesper.ringhog@gmail.com>
2025-10-30 11:19:35 +0100

    implemented remove/add to new list function/event

--- src/main.js
+++ src/main.js
@@ -1,75 +1,59 @@
 import "./style.css";
 
 const todos = [
-    "G",
-    "Skapa en lista", 
-    "Presentera listan på skärmen",
-    "Klickhändelse/ta bort todo/markera som klar",
-    "Implementera ett grafiskt ramverk",
-    "VG",
-    "Visa klara todos i ny lista",
     "Skapa formulär för nya todos",
     "Lägg till möjligheten att kunna sortera",
 ];
 
-//funktion för att lägga till klara uppgifter i ny lista
-const createHtmlForDones = () => {
-    const ulDones = document.createElement("ul");
-
-    ulDones.innerHTML = "";
-    ulDones.className = "m-5";
-
-    todos.forEach((todo) => {
-        const inputDone = document.createElement("input");
-        const liDone = document.createElement("li");
-        const divDone = document.createElement("div");
-
-        inputDone.type = "checkbox";
-        liDone.innerHTML = todo;
-        liDone.style.listStyle = "none";
-        divDone.classList = "d-flex gap-3 m-3";
-
-        divDone.append(inputDone, liDone);
-        ulDones.appendChild(divDone);
-    });
-    
-    app.appendChild(ulDones);
-};
+const dones = [];
 
 
-//funktion för att visa Todos
 const createHtmlForTodos = () => {
-    const app = document.getElementById("app");
+    const todosContainer = document.getElementById("todosContainer");
     const ul = document.createElement("ul");
 
-    app.innerHTML = "";
-    ul.className = "m-5";
+    todosContainer.innerHTML = "";
 
     todos.forEach((todo, i) => {
-        const input = document.createElement("input");
         const li = document.createElement("li");
-        const div = document.createElement("div");
 
-        input.type = "checkbox";
         li.innerHTML = todo;
-        li.style.listStyle = "none";
-        div.classList = "d-flex gap-3 m-3";
 
-        input.addEventListener("click", () => {
+        li.addEventListener("click", () => {
+            dones.push(todos[i]);
             todos.splice(i, 1);
-            console.log(todos);
+
             createHtmlForTodos();
-
-            //anropa funktion för att lägga till klara uppgifter i ny lista
+            createHtmlForDones();
         });
-
-        div.append(input, li);
-        ul.appendChild(div);
+        
+        ul.appendChild(li);
     });
     
-    app.appendChild(ul);
+    todosContainer.appendChild(ul);
+};
+
+const createHtmlForDones = () => {
+    const donesContainer = document.getElementById("donesContainer");
+    const ulDones = document.createElement("ul");
+    donesContainer.innerHTML = "";
+
+    dones.forEach((done, i) => {
+        const liDones = document.createElement("li");
+        liDones.innerHTML = done;
+        ulDones.appendChild(liDones);
+
+        liDones.addEventListener("click", () => {
+            todos.push(dones[i]);
+            dones.splice(i, 1);
+
+            createHtmlForTodos();
+            createHtmlForDones();
+        });
+    });
+    
+    donesContainer.appendChild(ulDones);
 };
 
 createHtmlForTodos();
-
 createHtmlForDones();
